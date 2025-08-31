a287460bbe1b91b37ba1b8a1c0fb90e396a68317
andrewlai <andrewlai@gmail.com>
2025-08-31 03:46:40 +0000

    Deploying to gh-pages from @ andrewlai/andrewlai.github.io@ca0b80af00f51e19b4fda6368b57a95d7af9e6a8 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "andrewlai/andrewlai",
+    "data-repo": "andrewlai/andrewlai.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
