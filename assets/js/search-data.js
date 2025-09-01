97144fd1683bcb87ffff01fb8c080656a0343341
andrewlai <andrewlai@gmail.com>
2025-09-01 05:27:58 +0000

    Deploying to gh-pages from @ andrewlai/andrewlai.github.io@e0143460a7b79e7245aab0806a078d6f67b5a2e6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -396,10 +396,7 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "news-new-site",
+            },},{id: "news-new-site",
           title: '✨ 😄 New site!',
           description: "",
           section: "News",},{id: "projects-project-1",
